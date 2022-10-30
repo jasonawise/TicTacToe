a18a6cd304ff25873c7c3c9d33f4e79400ea1250
Jason A. Wise <jasonandrewwise@gmail.com>
2022-10-30 15:54:39 -0400

    Initial Commit

---
+++ TicTacToe/TicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeApp.swift
+//  TicTacToe
+//
+//  Created by Jason Wise on 10/30/22.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
